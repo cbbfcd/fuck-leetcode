923a98edc01e57bea4b4f2113df4dbd21817cd0a
cbbfcd <2890636389@qq.com>
2021-05-26 14:39:03 +0800

    feat: update two_sum

--- src/two_sum.rs
+++ src/two_sum.rs
@@ -4,7 +4,7 @@ use std::collections::HashMap;
 
 impl Solution {
 
-  // 哈希表解法，复杂度 O(n)
+  // 哈希表解法，空间换时间，复杂度 O(n)
   pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     let mut map = HashMap::with_capacity(nums.len());
     for (index, num) in nums.iter().enumerate() {
@@ -14,6 +14,29 @@ impl Solution {
       }
     }
     vec![]
+  }
+
+  // 暴力解法
+  pub fn two_sum_force(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let len = nums.len();
+    for i in 0..len {
+      for j in i+1..len {
+        if nums[i] + nums[j] == target {
+          return vec![i as i32, j as i32];
+        }
+      }
+    }
+    vec![]
+  }
+
+  // 使用 iter 的语言特性去解
+  pub fn two_sum_iter(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut index: usize = 0;
+    let pos = nums.iter().enumerate().filter_map(|(i, num)| {
+      index = i;
+      nums[i+1..].iter().position(|&x| x == target - num)
+    }).next().unwrap() + index + 1;
+    vec![index as i32, pos as i32]
   }
 }
 
@@ -26,5 +49,7 @@ mod test {
   #[test]
   fn test() {
     assert_eq!(Solution::two_sum(vec![2, 7, 11, 5], 9), vec![0, 1]);
+    assert_eq!(Solution::two_sum_force(vec![2, 7, 11, 5], 9), vec![0, 1]);
+    assert_eq!(Solution::two_sum_iter(vec![2, 7, 11, 5], 9), vec![0, 1]);
   }
 }
